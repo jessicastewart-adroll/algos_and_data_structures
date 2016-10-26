0951c71dd9ec1b874a931811c202229f87e74935
Jess Stewart <jeselistewart@gmail.com>
2016-10-26 08:51:16 -0700

    Create pair_sum.py

---
+++ pair_sum.py
@@ -0,0 +1,5 @@
+def pairSum(int_array, total):
+    '''Find unique pairs of integers that sum to a value.
+    input: [50,50,60,40,90,20,10,80,13,12,10,87,97,3,50,80,20], 100
+    output: [(50,50),(60,40),(90,10),(80,20),(97,3)]
+    '''
