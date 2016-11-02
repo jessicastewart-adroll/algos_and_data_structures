a5750b88608e003807d9a0c14aa05fe64cadccf0
Jess Stewart <jeselistewart@gmail.com>
2016-11-02 07:42:11 -0700

    Update reverse_linked_list.py

--- reverse_linked_list.py
+++ reverse_linked_list.py
@@ -1,1 +1,9 @@
 '''Reverse a singly linked list.'''
+class Node(object):
+  def __init__(self, data):
+    self.data = data
+    self.next = None
+    
+class LinkedList(object):
+  def __init__(self):
+    self.head = None
