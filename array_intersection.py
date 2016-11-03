1e9e0035320d3f9eff366ba83f17bd058bd2d05a
Jess Stewart <jeselistewart@gmail.com>
2016-11-03 08:19:05 -0700

    Update array_intersection.py

--- array_intersection.py
+++ array_intersection.py
@@ -1,5 +1,14 @@
-'''
-Intersection of 2 arrays
-'''
+from collections import defaultdict
+
 def get_intersection(array_1, array_2):
-    pass
+    '''Intersection of 2 arrays'''
+    counts = defaultdict(int)
+    
+    array = array_1 + array_2
+    
+    for value in array:
+        counts[value] +=1
+        
+    return [value for value, count in counts.items() if count == 2]    
+        
+assert get_intersection([0, 3, 5, 9, 10], [-1, 3, 5, 7, 11]) == [3, 5]  
