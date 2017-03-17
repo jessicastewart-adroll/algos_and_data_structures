8a4648ff0206c0fdb9509bb61c32572dbbfacf99
Jess Stewart <jeselistewart@gmail.com>
2017-03-17 07:15:43 -0700

    Update tic_tac_toe.js

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -12,32 +12,31 @@ var board = [ [null, null, null],
 var rl = readline.createInterface(process.stdin, process.stdout);
 var currentPlayer = Object.keys(players)[0];
 
+function printBoard(board) {
+  for (var i = 0; i < board.length; i++) {
+    console.log(board[i]);
+  }
+}
+
 rl.setPrompt(currentPlayer + ' ');
-console.log(board);
+printBoard(board);
 rl.prompt();
 rl.on('line', function(line) {
-    if (!line.split(' ').length == 2) {
-      console.log('Bad input');
-      rl.prompt();
-      return
-    }
-
     var x = line.split(' ')[0].toString();
     var y = line.split(' ')[1].toString();
-    if (!x || !y || x<0 || y<0 || x>=board.length || y>=board[0].length) {
-      console.log('Bad input');
-      rl.prompt();
+
+    if (!board[x][y] ) {
+      board[x][y] = players[currentPlayer].pop()
+      currentPlayer = currentPlayer == 'playerOne' ? Object.keys(players)[1] : Object.keys(players)[0] 
     }
-
-    board[x][y] = players[currentPlayer].pop()
-
-    currentPlayer = currentPlayer == 'playerOne' ? Object.keys(players)[1] : Object.keys(players)[0] 
+    
     rl.setPrompt(currentPlayer + ' ');
 
     if (!players.playerOne.length && !players.playerTwo.length) {
+      printBoard(board);
       rl.close();
     } else {
-      console.log(board);
+      printBoard(board);
       rl.prompt();
     }
 }).on('close',function(){
