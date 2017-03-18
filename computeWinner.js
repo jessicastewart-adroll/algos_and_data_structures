4be9a679388e38b32db23aa214a25b0db19d576e
Jessica Stewart <jessica.stewart@adroll.com>
2017-03-18 08:29:52 -0700

    Update and rename tic_tac_toe.js to computeWinner.js

--- computeWinner.js
+++ computeWinner.js
@@ -1,3 +1,100 @@
+///////////////// final ////////////////////////
+function computeWinner(board, player) {
+  for (var i = 0; i < board[0].length; i++) {
+    var rows = search(board, player, i, 0);
+    var columns = search(board, player, 0, i);
+    var values = [];
+    for (var direction in rows) {
+      if (rows[direction] == board.length || columns[direction] == board.length) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+function search(board, player, row, column, counts) {
+  if (!counts) {
+    var counts = {
+      horizontal: 0,
+      vertical: 0,
+      forward: 0,
+      backward: 0
+    };
+  }
+  if (row < 0 || column < 0 || row >= board.length || column >= board.length) {
+    return counts;
+  }
+
+  if (board[row][column] != player) {
+    return counts;
+  }
+
+  counts.horizontal = 1 + search(board, player, row+1, column, counts).horizontal;
+  counts.vertical = 1 + search(board, player, row, column+1).vertical;
+  counts.forward = 1 + search(board, player, row+1, column+1).forward;
+  counts.backward = 1 + search(board, player, row-1, column+1).backward;
+  return counts;
+}
+
+var testHorizontal = [ 
+                   [00, 01, 02],
+                   ['x', 'x', 'x'],
+                   [20, 21, 22],
+                ];
+
+var testVeritcal = [ 
+                   [00, 'x', 02],
+                   [10, 'x', 12],
+                   [20, 'x', 22],
+                ];
+
+var testForwardDiagonal = [ 
+                   ['x', 01, 02],
+                   [10, 'x', 12],
+                   [20, 21, 'x'],
+                ];
+
+var testBackwardDiagonal = [ 
+                   [00, 01, 'x'],
+                   [10, 'x', 12],
+                   ['x', 21, 22],
+                ];
+
+var testBrokenOne = [ 
+                   [00, 01, 'x'],
+                   [10, 'x', 12],
+                   [20, 'x', 22],
+                ];
+
+var testBrokenTwo = [ 
+                   [00, 01, 'x'],
+                   [10, 'x', 'x'],
+                   [20, 21, 22],
+                ];
+
+var testBrokenThree = [ 
+                   [00, 01, 'x'],
+                   [10, 'x', 12],
+                   [20, 21, 'x'],
+                ]; 
+
+var twoPlayers = [
+                [ 'o', 'x', 'o' ],
+                [ null, 'x', null ],
+                [ null, null, 'x' ], 
+                ];
+
+console.log(computeWinner(testHorizontal, 'x'));  
+console.log(computeWinner(testVeritcal, 'x')); 
+console.log(computeWinner(testForwardDiagonal, 'x'));
+console.log(computeWinner(testBackwardDiagonal, 'x'));   
+console.log(computeWinner(testBrokenOne, 'x'));
+console.log(computeWinner(testBrokenTwo, 'x')); 
+console.log(computeWinner(testBrokenThree, 'x'));
+console.log(computeWinner(twoPlayers, 'x')); 
+console.log(computeWinner(twoPlayers, 'o')); 
+
 ///////////////// dfs ////////////////////////
 function compute_winner(board, player) {
   for (var i = 0; i < board[0].length; i++) {
