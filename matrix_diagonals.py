bd3a1d924119b7f1e5cd2bf6b4a01cfce920386f
Jess Stewart <jeselistewart@gmail.com>
2017-03-25 09:39:37 -0700

    Update matrix_diagonals.py

--- matrix_diagonals.py
+++ matrix_diagonals.py
@@ -1,42 +1,53 @@
 '''
-GIVEN: MxN
-[[9 3 2]
-[8 6 1]
-[5 5 6]
-[1 2 8]]
-
-OUTPUT:
-9      
-3 8 
-2 6 5  
-1 5 1 
-6 2  
-8  
-
-  00
-  01 10
-  02 11 20
-  12 21 30
-  22 31 
-  23
+[[1, 2],
+[3, 4]]
+1
+3 2
+4
 '''
-def print_backwards_diagonals(matrix):
-  m = len(matrix)
-  n = len(matrix[0])
+
+def square_forward(matrix):
+	size = len(matrix)
+	if size != len(matrix[0]): 
+		print("Input square")
+		return
   
-  output_rows = m + n - 1
-  
-  for row in range(0, output_rows):
-    out = 0
-    while out < n:
-      i = 0
-      j = 0
-    print(row)
-    matrix[i][j]
-    j += 1
-    
-test = [[9, 3, 2],
-[8, 6, 1],
-[5, 5, 6],
-[1, 2, 8]]
-print_backwards_diagonals(test) 
+  ## top
+	# y = 0
+	# while y < size:
+	# 	x = 0
+	# 	to_print = []
+	# 	while x <= y:
+	# 		to_print.append(str(matrix[y-x][x]))	
+	# 		x += 1
+	# 	print(' '.join(to_print))
+	# 	y += 1
+
+  ## bottom
+	x = 1
+	while x < size-1:
+		y = size-1
+		print(x, y)
+		to_print = []
+		while x-y > size:
+			print(x, y)
+			to_print.append(str(matrix[y][x-y]))	
+			y -= 1
+		print(' '.join(to_print))
+		x += 1	
+
+
+test0 = [[]]
+test1 = [[1]]
+test2 = [
+[1, 2],
+[3, 4]]
+test3 = [
+[1, 2, 3],
+[4, 5, 6],
+[7, 8, 9]]
+
+# tests = [test0, test1, test2, test3]
+# for test in tests:
+# 	
+square_forward(test3)
