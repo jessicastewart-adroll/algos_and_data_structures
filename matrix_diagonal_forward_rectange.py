abefb74e3c9d054de402b932db533c6a81a2ef60
Jess Stewart <jeselistewart@gmail.com>
2017-03-25 20:17:18 -0700

    Update matrix_diagonal_forward_rectange.py

--- matrix_diagonal_forward_rectange.py
+++ matrix_diagonal_forward_rectange.py
@@ -3,43 +3,45 @@ def rec_forward(matrix):
 	width = len(matrix[0])
 
 	y = 0
-	while y < width:
+	while y < height:
 		x = 0
 		to_print = []
-		while x <= y:
+		while x <= y and x < width:
 			to_print.append(str(matrix[y-x][x]))	
 			x += 1
 		print(' '.join(to_print))
 		y += 1
 
-	# x = 1
-	# while x < size:
-	# 	y = size-1
-	# 	to_print = []
-	# 	j = 1
-	# 	while x*j < size:
-	# 		to_print.append(str(matrix[y][x*j]))	
-	# 		y -= 1
-	# 		j += 1
-	# 	print(' '.join(to_print))
-	# 	x += 1	
+	x = 1
+	while x < width:
+		y = height-1
+		to_print = []
+		j = 1
+		while y >= 0 and x*j < width:
+			row = x*j
+			to_print.append(str(matrix[y][row]))	
+			y -= 1
+			j += 1
+		print(' '.join(to_print))
+		x += 1	
 
 
-# test0 = [[]]
-# test1 = [[1]]
-# test2 = [
-# [1, 2],
-# [3, 4],
-# [5, 6]]
+test0 = [[]]
+test1 = [[1]]
+test2 = [
+[1, 2],
+[3, 4],
+[5, 6]]
 test3 = [
 [1, 2, 3],
 [4, 5, 6]]
-# test4 = [
-# [1, 2, 3],
-# [4, 5, 6],
-# [7, 8, 9],
-# [10, 11, 12]]
+test4 = [
+[1, 2, 3],
+[4, 5, 6],
+[7, 8, 9],
+[10, 11, 12]]
 
-#tests = [test0, test1, test2, test3, test4]
-# for test in tests:
-rec_forward(test3)
+tests = [test0, test1, test2, test3, test4]
+for test in tests:
+	rec_forward(test)
+	print('\n')
