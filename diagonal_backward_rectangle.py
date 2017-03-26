656e4c2ad2d1632c9940454fffefff407c03faf0
Jess Stewart <jeselistewart@gmail.com>
2017-03-26 10:54:46 -0700

    Update diagonal_backward_rectangle.py

--- diagonal_backward_rectangle.py
+++ diagonal_backward_rectangle.py
@@ -1,1 +1,46 @@
+def rec_backward(matrix):
+	width = len(matrix[0])
+	height = len(matrix)
 
+	x = 0
+	while x < width:
+		y = 0
+		diagonal = []
+		while y <= x and y < height:
+			diagonal.append(str(matrix[y][x-y]))
+			y+=1
+		print(' '.join(diagonal))
+		x+=1
+
+	y = 1
+	while y < height:
+		x = width-1
+		diagonal = []
+		col_count = 1
+		while y*col_count < height and x >= 0:
+			diagonal.append(str(matrix[y*col_count][x]))
+			x-=1
+			col_count += 1
+		print(' '.join(diagonal))
+		y+=1	
+
+
+test0 = [[]]
+test1 = [[1]]
+test2 = [
+[1, 2, 3],
+[3, 4, 6]]
+test3 = [
+[1, 2],
+[3, 4],
+[5, 6]]
+test4 = [
+[1],
+[2],
+[3],
+[4]]
+
+tests = [test0, test1, test2, test3, test4]
+for test in tests:
+	rec_backward(test)
+	print('\n')
