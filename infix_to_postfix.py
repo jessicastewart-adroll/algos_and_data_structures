491e000a7f460d3c559b5facf403b7f15d77cc63
Jess Stewart <jeselistewart@gmail.com>
2017-04-18 13:08:18 -0700

    Update infix_to_postfix.py

--- infix_to_postfix.py
+++ infix_to_postfix.py
@@ -1,31 +1,55 @@
 operator_precedence = {
-  '^': 1,
-  '*': 2,
-  '/': 2, 
-  '+': 3,
-  '-': 3,  
+  ')': 3,
+  '(': 3,
+  '^': 2,
+  '*': 1,
+  '/': 1, 
+  '+': 0,
+  '-': 0,  
 }
 
 def infix_to_postfix(infix):
   stack = []
-  for i in infix:
-    if type(i) == int:
+  current = 0
+  while current < len(infix):
+    i = infix[current]
+    if i not in operator_precedence.keys():
       print(i)
-    elif i == '(':
-      stack.append(i)
-    elif i == ')':
-      found = False
-      while not found: 
+    elif not stack or '(' in stack:
+      if i == ')':
         val = stack.pop()
-        print(val)
-        if val == '(':
-          found = True
-    else:
-      if not stack or operator_precedence.get(i, 0) < operator_precedence.get(stack[-1], 0):
+        while val != '(': 
+          print(val)
+          val = stack.pop()
+      else:    
         stack.append(i)
-      else:
-        while operator_precedence.get(i, 0) > operator_precedence.get(stack[-1], 0):
-          print(stack.pop())
+    elif i == '(':
+      stack.append(i)  
+    elif operator_precedence[i] > operator_precedence[stack[-1]]:
+      stack.append(i)    
+    elif operator_precedence[i] == operator_precedence[stack[-1]]:
+      val = stack.pop()
+      if val not in [')', '(']:
+        print(val)
+      stack.append(i)
+    elif operator_precedence[i] < operator_precedence[stack[-1]]:
+      val = stack.pop()
+      if val not in [')', '(']:
+        print(val)
+        current-=1
+    current+=1
         
-infix_example = [1, '+', 2, '*', '(', 3, '^', 4, '-', 5, ')', '^', '(', 6, '+', 7, '*', 8, ')', '-', 9]   
-infix_to_postfix(infix_example)
+
+  while stack:
+    val = stack.pop()
+    if val not in [')', '(']:
+      print(val)
+      
+        
+infix_one = ['A', '*', 'B', '+', 'C']  
+infix_two = ['A', '+', 'B', '*', 'C']  
+infix_three = ['A', '*', '(', 'B', '+', 'C', ')'] 
+infix_four = ['A', '-', 'B', '+', 'C']  
+infix_five = ['A', '*', 'B', '^', 'C', '+', 'D']
+infix_six = ['A', '*', '(', 'B', '+', 'C', '*', 'D', ')', '+', 'E']
+infix_to_postfix(infix_six)
