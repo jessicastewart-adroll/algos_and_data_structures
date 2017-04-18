c88a0903bd067145d766380c5813b0e6008d83a4
Jess Stewart <jeselistewart@gmail.com>
2017-04-18 16:37:35 -0700

    Update infix_to_postfix.py

--- infix_to_postfix.py
+++ infix_to_postfix.py
@@ -1,3 +1,4 @@
+# please excuse my dear aunt sally
 operator_precedence = {
   ')': 3,
   '(': 3,
@@ -11,31 +12,38 @@ operator_precedence = {
 def infix_to_postfix(infix):
   stack = []
   current = 0
+  final = []
+  # iterate over expression
   while current < len(infix):
     i = infix[current]
+    # only care about operators 
     if i not in operator_precedence.keys():
-      print(i)
+      final.append(i)
     elif not stack or '(' in stack:
+      # keep acruing until end of parens 
       if i == ')':
         val = stack.pop()
         while val != '(': 
-          print(val)
+          final.append(val)
           val = stack.pop()
       else:    
         stack.append(i)
     elif i == '(':
       stack.append(i)  
+    # save greater preced   
     elif operator_precedence[i] > operator_precedence[stack[-1]]:
-      stack.append(i)    
+      stack.append(i)  
+    # pop less preced    
     elif operator_precedence[i] == operator_precedence[stack[-1]]:
       val = stack.pop()
       if val not in [')', '(']:
-        print(val)
+        final.append(val)
       stack.append(i)
     elif operator_precedence[i] < operator_precedence[stack[-1]]:
       val = stack.pop()
       if val not in [')', '(']:
-        print(val)
+        final.append(val)
+        # compare again
         current-=1
     current+=1
         
@@ -43,7 +51,7 @@ def infix_to_postfix(infix):
   while stack:
     val = stack.pop()
     if val not in [')', '(']:
-      print(val)
+      final.append(val)
       
         
 infix_one = ['A', '*', 'B', '+', 'C']  
@@ -52,4 +60,4 @@ infix_three = ['A', '*', '(', 'B', '+', 'C', ')']
 infix_four = ['A', '-', 'B', '+', 'C']  
 infix_five = ['A', '*', 'B', '^', 'C', '+', 'D']
 infix_six = ['A', '*', '(', 'B', '+', 'C', '*', 'D', ')', '+', 'E']
-infix_to_postfix(infix_six)
+postfix = infix_to_postfix(infix_five)
