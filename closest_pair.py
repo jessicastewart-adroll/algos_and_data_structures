207fbc2a77b012c2a9292e394ea9eb176b3de754
Jess Stewart <jeselistewart@gmail.com>
2017-04-22 11:47:36 -0700

    Update closest_pair.py

--- closest_pair.py
+++ closest_pair.py
@@ -1,3 +1,37 @@
+### steps 1-3
+import math 
+
+def brute_force(pairs):
+  min_distance = float('inf')
+  i = 0
+  while i < len(pairs):
+    j = i+1
+    while j < len(pairs):
+      a = pairs[i]
+      b = pairs[j]
+      distance = math.sqrt(pow(a[0]-b[0], 2) + pow(b[1]-b[1], 2))
+      min_distance = min(min_distance, distance)
+      j += 1
+    i += 1  
+  return min_distance
+
+def closest_pair(pairs):
+  n = len(pairs)
+  if n <= 3:
+    return brute_force(pairs)
+  
+  x_sorted_pairs = sorted(pairs)
+  print(x_sorted_pairs)
+  m = n//2
+  left = pairs[:m]
+  right = pairs[m:]
+  ld = closest_pair(left)
+  rd = closest_pair(right)
+  print(ld, rd)
+  
+pairs_test = [(2, 3), (12, 30), (40, 50), (5, 1), (12, 10), (3, 4)]  
+closest_pair(pairs_test)
+
 ### brute force
 from collections import defaultdict
 from math import sqrt
