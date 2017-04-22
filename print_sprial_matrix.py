0d06ed96c6a32ff1a9dbc7788b463721ef8c7b6d
Jess Stewart <jeselistewart@gmail.com>
2017-04-22 08:11:31 -0700

    Update print_sprial_matrix.py

--- print_sprial_matrix.py
+++ print_sprial_matrix.py
@@ -6,35 +6,40 @@ def print_spiral(matrix):
   x = 0
   y = 0
   
-  while left_boundary <= right_boundary or top_boundary <= bottom_boundary:
-    print('to right')
+  while left_boundary < right_boundary or top_boundary < bottom_boundary:
+    level = []
     while x < right_boundary:
-      print(matrix[y][x])
+      level.append(matrix[y][x])
       x += 1
     y += 1
     x -= 1
-    right_boundary -= 1
-    print('to bottom')
+    top_boundary += 1
+    print(level)
+    level = []
     while y < bottom_boundary:
-      print(matrix[y][x])
+      level.append(matrix[y][x])
       y += 1
     x -= 1
     y -= 1
-    bottom_boundary -= 1
-    print('to left')
+    right_boundary -= 1
+    print(level)
+    level = []
     while x >= left_boundary:
-      print(matrix[y][x])
+      level.append(matrix[y][x])
       x -= 1
     y -= 1
     x += 1
-    left_boundary += 1
-    print('to top')
+    bottom_boundary -= 1
+    print(level)
+    level = []
     while y >= top_boundary:
-      print(matrix[y][x])
+      level.append(matrix[y][x])
       y -= 1
     x += 1
-    top_boundary += 1
+    y += 1
+    left_boundary += 1
+    print(level)
   
 matrix_odd = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
 matrix_even = [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11], [12, 13, 14, 15]]
-print_spiral(matrix_odd)
+print_spiral(matrix_even)
